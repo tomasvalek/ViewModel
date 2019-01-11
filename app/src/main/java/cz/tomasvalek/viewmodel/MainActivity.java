449466798295615e1ac1400ed937a9065413e337
Tomas Valek <tomaskocz@seznam.cz>
2019-01-11 11:21:15 +0100

    Initial commit.

---
+++ app/src/main/java/cz/tomasvalek/viewmodel/MainActivity.java
@@ -0,0 +1,20 @@
+package cz.tomasvalek.viewmodel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cz.tomasvalek.viewmodel.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.container, MainFragment.newInstance())
+					.commitNow();
+		}
+	}
+}
---
+++ app/src/main/java/cz/tomasvalek/viewmodel/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package cz.tomasvalek.viewmodel.ui.main;
+
+import android.arch.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+	// TODO: Implement the ViewModel
+}
